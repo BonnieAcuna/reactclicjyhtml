674e8acb19d66d1004074344b9391e4b2c733930
Bonnie <bonnieacuna@gmail.com>
2019-03-17 10:15:31 -0700

    first commit

---
+++ precache-manifest.2f56feb386294eb22b829094f1e074aa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb2944186b0472a7c8438d39cbbccd2f",
+    "url": "/static/media/supreme.fb294418.jpg"
+  },
+  {
+    "revision": "39721157f45950a285b0",
+    "url": "/static/css/main.f7d2a7ae.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6eb8a6f83ee1a336e756",
+    "url": "/static/js/2.cb78a72a.chunk.js"
+  },
+  {
+    "revision": "54e253eeb04276311b607b6011ff5177",
+    "url": "/static/media/pepperoni.54e253ee.jpeg"
+  },
+  {
+    "revision": "c1bf781e4ad73f6bb535ea6a2a81acc5",
+    "url": "/static/media/sausage.c1bf781e.jpg"
+  },
+  {
+    "revision": "9752a40309cfe3cfe445eafa67de0f17",
+    "url": "/static/media/hawaiian.9752a403.jpg"
+  },
+  {
+    "revision": "1daf3837782992c0d38069bf246eeb35",
+    "url": "/static/media/margherita.1daf3837.jpeg"
+  },
+  {
+    "revision": "39721157f45950a285b0",
+    "url": "/static/js/main.18d55c80.chunk.js"
+  },
+  {
+    "revision": "208b49698e1fc680e4659748190a002c",
+    "url": "/static/media/vegetarian.208b4969.jpg"
+  },
+  {
+    "revision": "f1079cc4d35fa8b3522d57e4ec10bec0",
+    "url": "/static/media/pesto.f1079cc4.jpeg"
+  },
+  {
+    "revision": "9b4107436cfdbaaa24302060e9c22c62",
+    "url": "/static/media/shrimp.9b410743.jpeg"
+  },
+  {
+    "revision": "05c0b9fdb83863ebcf22ac397d57092c",
+    "url": "/static/media/bbqchicken.05c0b9fd.jpg"
+  },
+  {
+    "revision": "c569d60e387322f8e859436ab6c3ec4c",
+    "url": "/static/media/pickle.c569d60e.jpeg"
+  },
+  {
+    "revision": "c7aa50ba321505aecd1c29ec8f1505fa",
+    "url": "/static/media/potato.c7aa50ba.jpg"
+  },
+  {
+    "revision": "9fde44e9e6a11c0b44cf1ce0e819097d",
+    "url": "/static/media/cheese.9fde44e9.jpg"
+  },
+  {
+    "revision": "7feee495c2095c71c75e42cfd16ef957",
+    "url": "/index.html"
+  }
+];
